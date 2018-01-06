70937ad4dbe5086779bc3e1fb0c220b77c39031d
Erik Cox <erikbcox@gmail.com>
2018-01-05 18:11:15 -0800

    Simplify control flow

--- task.js
+++ task.js
@@ -1,7 +1,5 @@
 #!/usr/bin/env node
 
-console.log("Starting Github Streaker!");
-
 var dotenv  = require('dotenv');
 dotenv.load();
 
@@ -30,36 +28,30 @@ function warnOfImpendingStreakDoom() {
     text:     'Your GitHub streak is about to break. Go and make a commit quick!'
   }, function(err, json) {
     if (err) { return console.error(err); }
-    console.log(json);
   });
 }
 
 request(request_options, function (error, response, body) {
-  console.log("Starting request with status: ", response.statusCode);
   if (!error && response.statusCode == 200) {
-    console.log("Status 200");
     var json = JSON.parse(body);
     var today = new Date().getUTCDate();
     var count = 0;
 
     for (key in json) {
-        if (json.hasOwnProperty(key)) {
-          if (json[key].type === "PushEvent") {
+        if (json.hasOwnProperty(key) && json[key].type === "PushEvent") {
             var date = new Date(json[key].created_at).getUTCDate();
             if (date === today) {
               count++;
             }
-          }
         }
     }
 
-    console.log("Count: ", count);
     if (count <= 0) {
       warnOfImpendingStreakDoom();
     } else {
       console.log(count + " commit(s) today");
     } 
   } else {
-    console.log("Status isn't 200. ", body, url);
+    console.log("Status from url " + url + " is: " + response.statusCode + ". Body: " + body);
   }
 });

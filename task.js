977158b9c229ad33c831b8501781f09e530b58c3
Erik Cox <erikbcox@gmail.com>
2018-01-07 17:06:17 -0800

    Add calendar-parser package for stats

--- task.js
+++ task.js
@@ -1,6 +1,10 @@
 #!/usr/bin/env node
 
 var dotenv  = require('dotenv');
+var parse = require("github-calendar-parser");
+var request   = require('request');
+var sendgrid  = require('sendgrid')(SENDGRID_USERNAME, SENDGRID_PASSWORD);
+
 dotenv.load();
 
 var GITHUB_USERNAME   = process.env.GITHUB_USERNAME;
@@ -9,48 +13,37 @@ var SENDGRID_PASSWORD = process.env.SENDGRID_PASSWORD;
 var TO                = process.env.TO;
 var FROM              = process.env.FROM || TO;
 
-var request   = require('request');
-var sendgrid  = require('sendgrid')(SENDGRID_USERNAME, SENDGRID_PASSWORD);
-
 var request_options = {
-  url: 'https://api.github.com/users/' + GITHUB_USERNAME + '/events/public',
+  url: 'https://github.com/users/' + GITHUB_USERNAME + '/contributions',
   headers: {
     'User-Agent': 'request'
   }
 };
 
-function warnOfImpendingStreakDoom() {
+function warnOfImpendingStreakDoom(m) {
   console.log("Attempting to send email...");
   sendgrid.send({
     to:       TO,
     from:     FROM,
     subject:  "[github-streaker] Don't break the streak.",
-    text:     'Your GitHub streak is about to break. Go and make a commit quick!'
+    text:     `${message}\nYour GitHub streak is about to break. Go and make a commit quick!\n`
   }, function(err, json) {
     if (err) { return console.error(err); }
   });
 }
 
 request(request_options, function (error, response, body) {
+
   if (!error && response.statusCode == 200) {
-    var json = JSON.parse(body);
-    var today = new Date().getUTCDate();
-    var count = 0;
+    var contributions = parse(body);
+    var today = new Date().toISOString().split('T')[0];
 
-    for (key in json) {
-        if (json.hasOwnProperty(key) && json[key].type === "PushEvent") {
-            var date = new Date(json[key].created_at).getUTCDate();
-            if (date === today) {
-              count++;
-            }
-        }
+    var todays_count = contributions.days.find(o => o.date.toISOString().split('T')[0] === today)  || 0;
+    var message = `Your current streak is ${contributions.current_streak} days. Your longest streak is ${contributions.longest_streak} days.`;
+
+    if (todays_count <= 0) {
+      warnOfImpendingStreakDoom(message);
     }
-
-    if (count <= 0) {
-      warnOfImpendingStreakDoom();
-    } else {
-      console.log(count + " commit(s) today");
-    } 
   } else {
     console.log("Status from url " + url + " is: " + response.statusCode + ". Body: " + body);
   }
